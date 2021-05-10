d4e1d18c227498be5788ccc3723c07fe6224905e
paoloferrari97 <ferrari.paolo.97@gmail.com>
2021-05-10 16:14:02 +0200

    Snack 5

--- assets/js/main.js
+++ assets/js/main.js
@@ -124,3 +124,24 @@ for (var i = 0; i < arrayUno.length; i++){ //arrayUno.length = arrayDue.length
 }
 
 console.log(arrayComune);
+
+
+
+//Snack 5:
+//Scrivi una funzione che accetti tre argomenti:
+//un array e due numeri (“a” più piccolo di “b” e “b” grande al massimo 
+//quanto il numero di elementi dell’array).
+//La funzione ritornerà un nuovo array con i valori che hanno 
+//la posizione compresa tra “a” e “b”
+
+function snackCinque(array, numeroUno, numeroDue) {//numeroUno < numeroDue e numeroDue <= array.length
+    var nuovoArray = [];
+    for (var i = numeroUno; i <= numeroDue; i++){
+        nuovoArray.push(array[i]);
+    }
+    return nuovoArray;
+}
+
+var lista = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+
+console.log(snackCinque(lista, 6, 18)); //darà array con gli elementi di lista da posizione 6 a 18
